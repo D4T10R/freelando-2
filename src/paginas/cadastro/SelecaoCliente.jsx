ac89238563afa925df780a69ed60691111be39f7
Datior <leonardowederveiga@yahoo.com>
2023-04-16 00:20:17 -0300

    Rotas aninhadas

---
+++ src/paginas/cadastro/SelecaoCliente.jsx
@@ -0,0 +1,41 @@
+import { Col, Row } from "react-grid-system"
+import { Tipografia } from "../../componentes/Tipografia/Tipografia"
+
+import cliente from "./assets/Imagem cliente.svg"
+import freelancer from "./assets/Imagem freela.svg"
+import { Estilos } from "../../componentes/EstilosGlobais/Estilos"
+import { Link } from "../../componentes/Link/Link"
+
+
+const SelecaoCliente  = () => {
+    return (
+        <>
+            <Tipografia variante='h1' componente='h1'>
+                Crie seu cadastro
+            </Tipografia>
+            <Tipografia variante='h3' componente='h2'>
+                Como podemos te ajudar?
+            </Tipografia>
+            <Row>
+                <Col md={6} sm={12}>
+                    <img src={cliente} alt="" />
+                    <Tipografia variante='body' componente='body'>Sou cliente e preciso de um freela</Tipografia>
+                </Col>
+                <Col md={6} sm={12}>
+                    <img src={freelancer} alt="" />
+                    <Tipografia variante='body' componente='body'>Sou freela preciso de um cliente</Tipografia>
+                </Col>
+            </Row>
+            <div style={{textAlign: 'center'}}>
+                <Tipografia variante='body2' componente='body2'>
+                    já tem conta?
+                </Tipografia>
+                <p>
+                    <Link variante="secundaria">Faça login!</Link>
+                </p>
+            </div>
+        </>
+    )
+} 
+
+export default SelecaoCliente
